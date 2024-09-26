ef12232e87594fcfb80a6d5ee2caad1da832b9bf
HalilDayi <halildayi.013@gmail.com>
2024-09-26 16:10:44 +0300

    Static Files Uploaded

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+//MİDDLEWARES
+app.use(express.static('public'));
+
+
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo Description',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Sunucu port ${port} portunda başlatıldı...`);
+  console.log(`Sunucu ${port} portunda baslatildi...`);
 });
